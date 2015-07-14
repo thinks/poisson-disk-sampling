8eea8e52eeaa96ebca455ee0f74751398c8918e1
Tommy Hinks <tommy.hinks@gmail.com>
2015-07-14 18:22:26 +0200

    Update tests

--- test/include/thinks/testPoissonDiskSampling.hpp
+++ test/include/thinks/testPoissonDiskSampling.hpp
@@ -5,7 +5,7 @@
 #include <cstdint>
 #include <vector>
 
-#include "poissonDiskSampling.hpp"
+#include "../../../include/thinks/poissonDiskSampling.hpp"
 
 namespace thinks {
 namespace test {
@@ -23,35 +23,36 @@ public:
   Vec(T x) { std::fill_n(_data, N, x); }
   T& operator[](const std::size_t i) { return _data[i]; }
   const T& operator[](const std::size_t i) const { return _data[i]; }
-
 private:
   T _data[N];
 };
 
 typedef Vec<float, 2> Vec2f;
 
+//! Helper struct for keeping track of inputs to the algorithm.
+template <std::size_t N>
 struct Input
 {
-  Input(const float radius, const Vec2f& x_min, const Vec2f& x_max,
-        const std::uint32_t max_sample_attempts, const std::uint32_t seed)
+  Input(const float radius, const Vec<float, N>& x_min,
+        const Vec<float, N>& x_max, const std::uint32_t max_sample_attempts,
+        const std::uint32_t seed)
     : radius(radius), x_min(x_min), x_max(x_max)
     , max_sample_attempts(max_sample_attempts), seed(seed) {}
 
   float radius;
-  Vec2f x_min;
-  Vec2f x_max;
+  Vec<float, N> x_min;
+  Vec<float, N> x_max;
   std::uint32_t max_sample_attempts;
   std::uint32_t seed;
 };
 
-//!
-template <typename T, std::size_t N> inline
-typename Vec<T, N>::value_type
-squaredDistance(const Vec<T, N>& u, const Vec<T, N>& v)
+//! Returns the sqaured distance between the vectors @a u and @a v.
+template <typename V> inline
+typename V::value_type squaredDistance(const V& u, const V& v)
 {
   using namespace std;
   auto d = (u[0] - v[0]) * (u[0] - v[0]);
-  for (size_t i = 1; i < Vec<T, N>::size; ++i) {
+  for (size_t i = 1; i < V::size; ++i) {
     d += ((u[i] - v[i]) * (u[i] - v[i]));
   }
   return d;
@@ -59,7 +60,7 @@ squaredDistance(const Vec<T, N>& u, const Vec<T, N>& v)
 
 //! O(N^2) verification. Verifies that the distance between each possible
 //! sample pair meets the Poisson requirement, i.e. is less than some radius.
-template <typename V>
+template <typename V> inline
 bool verifyPoisson(const std::vector<V>& samples,
                    const typename V::value_type radius)
 {
@@ -74,30 +75,46 @@ bool verifyPoisson(const std::vector<V>& samples,
   return true;
 }
 
+//! Returns true if all points in @a samples are within the bounds specified
+//! by @a x_min and @a x_max.
+template <typename V> inline
+bool verifyBounds(const std::vector<V>& samples, const V& x_min, const V& x_max)
+{
+  using namespace std;
+  for (auto v = begin(samples); v != end(samples); ++v) {
+    for (size_t i = 0; i < V::size; ++i) {
+      if (x_min[i] > v[i] || v[i] > x_max[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
 } // namespace detail
 
+template <std::size_t N>
 inline bool testPoissonDiskSampling()
 {
   using namespace std;
 
-  vector<detail::Input> inputs;
-  inputs.push_back(detail::Input(2.f, detail::Vec2f(-10.f),
-                                 detail::Vec2f(10.f), 30, 0));
-  /*inputs.push_back(detail::Input(2.f, detail::Vec2f(-10.f),
+  vector<detail::Input<N>> inputs;
+  inputs.push_back(detail::Input(2.f, detail::Vec<float, N>(-10.f),
+                                 detail::Vec<float, N>(10.f), 30, 0));
+  inputs.push_back(detail::Input(5.f, detail::Vec2f(-10.f),
                                  detail::Vec2f(10.f), 30, 1981));
   inputs.push_back(detail::Input(3.f, detail::Vec2f(-5.f),
-                                 detail::Vec2f(20.f), 30, 2015));*/
+                                 detail::Vec2f(20.f), 30, 2015));
 
   for (auto input = begin(inputs); input != end(inputs); ++input) {
-    cerr << "Input: " << &*input << endl;
     auto samples = poissonDiskSampling(input->radius, input->x_min,
                                        input->x_max, input->max_sample_attempts,
                                        input->seed);
-    for (auto iter = begin(samples); iter != end(samples); ++iter) {
-      cerr << (*iter)[0] << ", " << (*iter)[1] << endl;
-    }
-    std::cerr << "Verifying..." << std::endl;
     if (!verifyPoisson(samples, input->radius)) {
+      return false;
+    }
+
+    if (!verifyBounds(samples, input->x_min, input->x_max)) {
       return false;
     }
   }
