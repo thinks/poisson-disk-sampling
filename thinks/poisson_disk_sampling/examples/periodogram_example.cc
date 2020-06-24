d301f968c9a7284bd75bea33fb1fe86a87280be4
Tommy Hinks <tommy.hinks@gmail.com>
2020-06-24 15:50:41 +0200

    comment clang-tidy hack

--- thinks/poisson_disk_sampling/examples/periodogram_example.cc
+++ thinks/poisson_disk_sampling/examples/periodogram_example.cc
@@ -270,6 +270,11 @@ static void WriteImage(const std::string& filename, const Image<double>& img) {
 
   const auto w = static_cast<int>(img.width());
   const auto h = static_cast<int>(img.height());
+
+  // HACK - clang-tidy will analyze this call and find errors in stb.
+  //        If we ignore this line it doesn't seem to analyze the stb code,
+  //        even though it is inlined in this file.
+  //
   // NOLINTNEXTLINE
   if (stbi_write_png(filename.c_str(), w, h, kComp, To8bits(img).data(), w) == 0) {
     std::cerr << "failed writing image";
