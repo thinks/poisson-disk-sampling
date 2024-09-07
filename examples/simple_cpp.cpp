ae5d2fb0a50fb9d1f10a0077e543d41336c49272
thinks <tommy.hinks@gmail.com>
2024-09-08 00:18:33 +0200

    readme

--- examples/simple_cpp.cpp
+++ examples/simple_cpp.cpp
@@ -1,59 +1,69 @@
 #include <array>//std::array
+#include <cstdint>// UINT64_C, etc
 #include <cstdio>// std::printf
 #include <functional>// std::function
 #include <memory>// std::unique_ptr
 
 #define TPH_POISSON_IMPLEMENTATION
-#include <thinks/tph_poisson.h>
+//#define TPH_POISSON_REAL_TYPE double
+//#include <cmath>
+//#define TPH_POISSON_SQRT std::sqrt
+//#define TPH_POISSON_CEIL std::ceil
+//#define TPH_POISSON_FLOOR std::floor
+#include "thinks/tph_poisson.h"
 
 int main(int /*argc*/, char * /*argv*/[])
 {
   // clang-format off
   constexpr std::array<tph_poisson_real, 2> bounds_min{ 
-    static_cast<tph_poisson_real>(-100),
-    static_cast<tph_poisson_real>(-100) };
+    static_cast<tph_poisson_real>(-10),
+    static_cast<tph_poisson_real>(-10) };
   constexpr std::array<tph_poisson_real, 2> bounds_max{ 
-    static_cast<tph_poisson_real>(100),
-    static_cast<tph_poisson_real>(100) };
+    static_cast<tph_poisson_real>(10),
+    static_cast<tph_poisson_real>(10) };
   // clang-format on
 
+  // Configure arguments.
   tph_poisson_args args = {};
-  args.radius = 10.F;
-  args.ndims = 2;
+  args.radius = static_cast<tph_poisson_real>(3);
+  args.ndims = INT32_C(2);
   args.bounds_min = bounds_min.data();
   args.bounds_max = bounds_max.data();
-  args.max_sample_attempts = 30;
-  args.seed = 1981;
+  args.max_sample_attempts = UINT32_C(30);
+  args.seed = UINT64_C(1981);
+  // Using default allocator (libc malloc).
+  const tph_poisson_allocator *alloc = NULL;
 
+  // Create samples.
   using unique_poisson_ptr =
     std::unique_ptr<tph_poisson_sampling, std::function<void(tph_poisson_sampling *)>>;
   auto sampling = unique_poisson_ptr{ new tph_poisson_sampling{}, [](tph_poisson_sampling *s) {
                                        tph_poisson_destroy(s);
                                        delete s;
                                      } };
-
-  if (const int ret = tph_poisson_create(&args, /*alloc=*/nullptr, sampling.get());
+  if (const int ret = tph_poisson_create(&args, alloc, sampling.get());
       ret != TPH_POISSON_SUCCESS) {
-    printf("Failed creating Poisson sampling! Error code: %d\n", ret);
+    std::printf("Failed creating Poisson sampling! Error code: %d\n", ret);
     return EXIT_FAILURE;
   };
 
+  // Retrieve samples.
   const tph_poisson_real *samples = tph_poisson_get_samples(sampling.get());
   if (samples == nullptr) {
-    // This cannot really happen since we check the return code from tph_poisson_create.
-    printf("Failed getting Poisson samples!\n");
+    // Cannot happen since we check the return value from tph_poisson_create!
+    std::printf("Failed getting Poisson samples!\n");
     return EXIT_FAILURE;
   }
 
-  // Print sample positions.
+  // Print first and last sample positions.
   // clang-format off
-  printf("\nsimple_cpp:\n");
-  printf("sample[%td] = ( %.3f, %.3f )\n", 
+  std::printf("\nsimple_cpp:\n");
+  std::printf("sample[%td] = ( %.3f, %.3f )\n", 
     (ptrdiff_t)0, 
     (double)samples[0], 
     (double)samples[1]);
-  printf("...\n");
-  printf("sample[%td] = ( %.3f, %.3f )\n\n",
+  std::printf("...\n");
+  std::printf("sample[%td] = ( %.3f, %.3f )\n\n",
     sampling->nsamples - 1,
     (double)samples[(sampling->nsamples - 1) * sampling->ndims],
     (double)samples[(sampling->nsamples - 1) * sampling->ndims + 1]);
