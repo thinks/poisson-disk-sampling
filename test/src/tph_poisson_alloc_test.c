e0aec93d9aeeb4a1ab7af1e9585e0ad910c853b4
thinks <tommy.hinks@gmail.com>
2024-10-28 21:15:46 +0100

    clang-tidy

--- test/src/tph_poisson_alloc_test.c
+++ test/src/tph_poisson_alloc_test.c
@@ -156,9 +156,6 @@ static void test_destroyed_alloc(void)
   REQUIRE(alloc_ctx.num_frees >= 0);
 
   /* Free memory associated with sampling. */
-#ifdef __clang_analyzer__
-  [[clang::suppress]]
-#endif
   tph_poisson_destroy(&sampling);
 }
 
