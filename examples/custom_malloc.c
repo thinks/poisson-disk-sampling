2c64e84df3636e03ddc74c959bee85b69e0cb790
thinks <tommy.hinks@gmail.com>
2024-10-28 22:55:22 +0100

    custom_malloc

--- examples/custom_malloc.c
+++ examples/custom_malloc.c
@@ -65,7 +65,12 @@ int main(int argc, char *argv[])
 
   /* Retrieve samples. */
   const tph_poisson_real *samples = tph_poisson_get_samples(&sampling);
-  assert(samples != NULL);
+  if (samples == NULL) {
+    /* Cannot happen since we check the return value from tph_poisson_create! */
+    printf("Bad samples!\n");
+    tph_poisson_destroy(&sampling);
+    return EXIT_FAILURE;
+  }
 
   /* Print first and last sample positions. */
   /* clang-format off */
